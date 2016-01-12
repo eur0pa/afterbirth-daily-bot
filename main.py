4b81fd6b0f510d45911fc1de54f1736a170efc3f
eur0pa <eur0pa@users.noreply.github.com>
2016-01-12 21:02:38 +0100

    removed debug print

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import greedbuttapi
 if __name__ == '__main__':
 
     today = greedbuttapi.leaderboard.Leaderboard()
-    print today.winner
 
     print "today's winner is {winner} with {points} points!".format(
         winner=(today.winner['name']).encode('utf8'),
